1350ba79f681a943b42322917f27fb927f0a0502
Rohit Kumar Pradhan <rohitkumarpradhan2000@gmail.com>
2026-04-23 18:24:25 +0530

    Image Added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,5 +63,7 @@ dependencies {
 
     implementation("androidx.compose.runtime:runtime-livedata:1.10.3")
     implementation("io.coil-kt.coil3:coil-compose:3.4.0")
-    implementation("io.coil-kt.coil3:coil-compose:3.0.0")
+
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.4.0")
+
 }
